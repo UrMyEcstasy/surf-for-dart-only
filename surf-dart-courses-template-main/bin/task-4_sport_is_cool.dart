621a0d761559d1e136ae4b005ca477a162cadc1e
Maxim <tymbochkax@gmail.com>
2024-04-25 18:04:52 +0300

    Teams reformated

--- surf-dart-courses-template-main/bin/task-4_sport_is_cool.dart
+++ surf-dart-courses-template-main/bin/task-4_sport_is_cool.dart
@@ -1,10 +1,11 @@
-import 'dart:math';
+class Team {
+  final String name;
+  final List<Person> members;
 
-String chooseRandom(List<String> teams){
-
-  var i = Random().nextInt(teams.length -1);
-
-  return teams[i];
+  Team(
+    this.name,
+    this.members,
+  );
 }
 
 abstract class Person {
@@ -12,70 +13,147 @@ abstract class Person {
   final String surname;
   final int height;
   final int weight;
+  final Team team;
 
-  Person(
-      {required this.name,
-        required this.surname,
-        required this.height,
-        required this.weight
-      });
+  Person({
+    required this.name,
+    required this.surname,
+    required this.height,
+    required this.weight,
+    required this.team,
+  });
 
   @override
   String toString() {
     return 'Person{name: $name, surname: $surname}';
   }
 
-  void whoIAm(){
+  void whoIAm() {
     print('Hello, my name is $name $surname');
   }
 }
 
-
-class Boxer extends Person{
+class Boxer extends Person {
   int wins;
   int losses;
   int draws;
-  List<String> teams = ["Tiger muai thai", "City King Boxing", "Arena Don"];
-  Boxer({required super.name, required super.surname, required super.height, required super.weight,
-    required this.wins, required this.draws, required this.losses});
+
+  Boxer({
+    required this.wins,
+    required this.draws,
+    required this.losses,
+    required String name,
+    required String surname,
+    required int height,
+    required int weight,
+    required Team team,
+  }) : super(
+          name: name,
+          surname: surname,
+          height: height,
+          weight: weight,
+          team: team,
+        );
+
   @override
-  void whoIAm(){
+  void whoIAm() {
     super.whoIAm();
     print('my record is $wins - $losses - $draws');
-    print("my gym is ${chooseRandom(teams)}");
+    print("my gym is ${team.name}");
     print('I am the greatest boxer in the world\n');
   }
-
 }
 
-class Grappler extends Person{
-
+class Grappler extends Person {
   int wins;
   int losses;
   int draws;
   final String style;
 
-  List<String> teams = ['Jackson Wink', 'AKA', 'Kings MMA'];
-  Grappler({required super.name, required super.surname, required super.height, required super.weight,
-    required this.wins, required this.draws, required this.losses, required this.style});
+  Grappler({
+    required this.wins,
+    required this.draws,
+    required this.losses,
+    required this.style,
+    required String name,
+    required String surname,
+    required int height,
+    required int weight,
+    required Team team,
+  }) : super(
+          name: name,
+          surname: surname,
+          height: height,
+          weight: weight,
+          team: team,
+        );
+
   @override
-  void whoIAm(){
+  void whoIAm() {
     super.whoIAm();
     print('my style is $style');
-    print("my gym is ${chooseRandom(teams)}");
+    print("my gym is ${team.name}");
     print('I am the best grappler\n');
   }
-
 }
+
 void main() {
-
-  var boxer = Boxer(name: "Andrey", surname: "Balboa",
-      weight: 90, height: 190, wins: 10, losses: 3, draws: 1);
-  boxer.whoIAm();
-
-  print('-' * 40);
-
-  var grappler = Grappler(name: 'Demian', surname: 'Maya', height: 185,
-      weight: 80, wins: 28, draws: 11, losses: 0, style: "jiu - jujitsu");
-  grappler.whoIAm();
+  var teams = [
+    Team(
+      'Tiger muai thai',
+      [
+        Boxer(
+          name: "Andrey",
+          surname: "Balboa",
+          weight: 90,
+          height: 190,
+          wins: 10,
+          losses: 3,
+          draws: 1,
+          team: Team('Tiger muai thai', []),
+        ),
+        Grappler(
+          name: 'Demian',
+          surname: 'Maya',
+          height: 185,
+          weight: 80,
+          wins: 28,
+          draws: 11,
+          losses: 0,
+          style: "jiu - jujitsu",
+          team: Team('Tiger muai thai', []),
+        ),
+      ],
+    ),
+    Team(
+      'City King Boxing',
+      [
+        Boxer(
+            name: "Israel",
+            surname: "Adesanya",
+            weight: 83,
+            height: 193,
+            wins: 24,
+            losses: 3,
+            draws: 0,
+            team: Team('City King Boxing', [])),
+        Boxer(
+            name: "Alexandr",
+            surname: "Volkanovski",
+            weight: 65,
+            height: 167,
+            wins: 26,
+            losses: 4,
+            draws: 0,
+            team: Team('City King Boxing', [])),
+      ],
+    ),
+  ];
+  for (var team in teams) {
+    print('-' * 40);
+    print('Members of ${team.name}:\n');
+    for (var member in team.members) {
+      member.whoIAm();
+    }
+  }
 }
